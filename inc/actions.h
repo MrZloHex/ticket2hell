81cf855d818ec966cf68349e6fc9b090fc3bc8f8
MrZloHex <zlo.alex.it@gmail.com>
2024-07-13 18:51:00 +0300

    actions

---
+++ inc/actions.h
@@ -0,0 +1,13 @@
+#ifndef __ACTIONS_H__
+#define __ACTIONS_H__
+
+typedef enum Action_E
+{
+    ACT_DRAW_TRAIN,
+    ACT_DRAW_TICKET,
+    ACT_CLAIN_ROUTE
+} Action;
+
+
+
+#endif /* __ACTIONS_H__ */
---
+++ inc/commands.h
@@ -0,0 +1,6 @@
+#ifndef __COMMANDS_H__
+#define __COMMANDS_H__
+
+
+
+#endif /* __COMMANDS_H__ */
