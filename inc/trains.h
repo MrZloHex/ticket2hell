cd2b453026a00ad783dbd143da65353616eafe86
MrZloHex <zlo.alex.it@gmail.com>
2024-07-13 17:28:21 +0300

    headers

---
+++ inc/trains.h
@@ -0,0 +1,15 @@
+#ifndef __TRAINS_H__
+#define __TRAINS_H__
+
+#include "stdlib.h"
+
+
+const static size_t k_train_color_quantity = 12;
+const static size_t k_locomotive_quantity = 14;
+
+typedef struct Train_Deck_S
+{
+    size_t cards;
+} Train_Deck;
+
+#endif /* __TRAINS_H__ */
